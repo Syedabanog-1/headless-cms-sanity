414e1e6dcd55bd385a9eb3fd86ec43ccca43e6d8
syedabanog-1 <gulzar_bano@yahoo.com>
2025-01-08 11:02:15 +0500

    commit best ok

--- sanity.config.ts
+++ sanity.config.ts
@@ -15,8 +15,8 @@ import {structure} from './src/sanity/structure'
 
 export default defineConfig({
   basePath: '/studio',
-  projectId,
-  dataset,
+  projectId: 'x3mu26pf',
+  dataset: 'production',
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
